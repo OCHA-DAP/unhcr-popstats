b2b708996a7b57f4f5f6e97897d4a1214dcf51ee
David Megginson <david.megginson@gmail.com>
2019-02-04 17:22:48 -0500

    Apparently working (needs testing)

--- 20190204-add-quick-charts.py
+++ 20190204-add-quick-charts.py
@@ -58,32 +58,50 @@ def load_models(ckan):
                 logger.info("Loaded Quick Charts configuration for %s from %s", entry[0], entry[1])
                 break
         if entry[0] not in quickcharts_configurations:
-            logger.error("Failed to load model configuration for %s", entry[0])
-            exit(1)
-    print(quickcharts_configurations)
-    exit(0)
-        
+            # Oops! Couldn't find config.
+            raise Exception("Failed to load model configuration for {}".format(entry[0]))
 
-def add_quickcharts(package, dataset_type, iso3):
+def add_quickcharts(ckan, package, dataset_type, iso3):
     """Add Quick Charts to a dataset after a match.
     Invoked by try_patterns()
+    @param ckan: the CKAN API access object
     @param package: the CKAN package (dataset) structure
     @param dataset_type: a string identifying the dataset type (e.g. "refugees-originating")
     @param iso3: the ISO3 code for the country
     """
-    print(package["name"], dataset_type, iso3)
 
-def try_patterns(package):
+    logger.info("Adding Quick Charts to %s", package["name"])
+
+    # Set up the package to preview
+    package["dataset_preview"] = "first_resource",
+    package["has_quickcharts"] = True
+    ckan.call_action("package_update", package)
+
+    # Set the Quick Charts configuration
+    resource_id = package["resources"][0]["id"]
+    views = ckan.action.resource_view_list(id=resource_id)
+    for view in views:
+        # Find the Quick Charts view and set the configuration
+        if view["view_type"] == "hdx_hxl_preview":
+            view["hxl_preview_config"] = quickcharts_configurations[dataset_type]
+            ckan.call_action("resource_view_update", view)
+            return
+
+    # If we get to here, then we failed
+    raise Exception("Failed to find a Quick Charts view for {}".format(package["name"]))
+
+def try_patterns(ckan, package):
     """Match a dataset short name against all known patterns.
     Invoked by scan_datasets()
     If successful, extract the type and country ISO3 code and invoke
     add_quickcharts().
+    @param ckan: the CKAN API access object
     @param package: the CKAN package (dataset) structure
     """
     for pattern in PATTERNS:
         result = re.fullmatch(pattern, package["name"])
         if result:
-            add_quickcharts(package, result.group(1), result.group(2))
+            add_quickcharts(ckan, package, result.group(1), result.group(2))
             return
     logger.warning("Skipping %s", package["name"])
     
@@ -97,7 +115,7 @@ def scan_datasets(ckanurl, apikey, delay=1):
     crawler = ckancrawler.Crawler(ckan_url=ckanurl, apikey=apikey, delay=delay)
     load_models(crawler.ckan)
     for package in crawler.packages(fq="organization:unhcr"): # scan only UNHCR datasets
-        try_patterns(package)
+        try_patterns(crawler.ckan, package)
 
 #
 # Invoke as a command-line script using the info in config.py
